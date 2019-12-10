b63bbd2ed6fef1fb8b2ba244c8b42eff08949627
Nicolas Gordat <nicolas.gordat@some-email-provider.com>
2019-12-10 01:30:20 +0100

    Better exception handling

--- back/JWT-NetCore-Vue/Services/Emails/EmailService.cs
+++ back/JWT-NetCore-Vue/Services/Emails/EmailService.cs
@@ -176,7 +176,7 @@ namespace JWTNetCoreVue.Services.Emails
         this.Send(address, subject, body);
         return true;
       }
-      catch (Exception ex)
+      catch (SmtpCommandException ex)
       {
         // TODO
         this.Logger.LogCritical(ex, "Error while sending email.");
@@ -198,7 +198,7 @@ namespace JWTNetCoreVue.Services.Emails
         this.SendTemplate(address, templateName, values);
         return true;
       }
-      catch (Exception ex)
+      catch (SmtpCommandException ex)
       {
         this.Logger.LogCritical(ex, "Error while sending email.");
         return false;
