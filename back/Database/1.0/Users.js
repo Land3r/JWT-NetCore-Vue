d9d8753755d12d1476b1432ff2b01a671e205cc9
Nicolas Gordat <nicolas.gordat@some-email-provider.com>
2019-12-11 21:59:35 +0100

    Update User sample

--- back/Database/1.0/Users.js
+++ back/Database/1.0/Users.js
@@ -5,5 +5,7 @@ db.Users.insertOne({
 	LastName: "test",
 	Username: "test",
 	Email: "test@yopmail.com",
-	Password: "041e66a2acaf543611ee5a8f028f2689de3b2cd22f83233968486fb968a865c5483f963901294ff4efb4eda27dbf527fe62a0b7e7e69041b5707e3e061140432"
+	Password: "041e66a2acaf543611ee5a8f028f2689de3b2cd22f83233968486fb968a865c5483f963901294ff4efb4eda27dbf527fe62a0b7e7e69041b5707e3e061140432",
+	Active: true,
+	ActivationToken: null
 });
